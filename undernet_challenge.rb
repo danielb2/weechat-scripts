ebb49a725a328e62035905a01c244edfb51f4029
Daniel Bretoi <daniel@bretoi.com>
2013-10-17 15:06:27 -0700

    change description and attempt to get rid of resends

--- undernet_challenge.rb
+++ undernet_challenge.rb
@@ -4,7 +4,7 @@ require 'uri'
 
 SCRIPT_NAME    = 'undernet_challenge'
 SCRIPT_AUTHOR  = 'Daniel Bretoi <daniel@bretoi.com>'
-SCRIPT_DESC    = 'respond to undernet challenge when theres no identd'
+SCRIPT_DESC    = 'respond to undernet challenge when theres no identd. Example: Ident broken or disabled, to continue to connect you must type /QUOTE PASS 29079'
 SCRIPT_VERSION = '0.1.0'
 SCRIPT_LICENSE = 'BSD'
 SCRIPT_REPO    = 'https://github.com/danielb2/weechat-scripts'
@@ -31,7 +31,7 @@ end
  
  
 def notice_cb(data, signal, signal_data)
-  if signal_data.include? "Ident broken or disabled, to continue to connect you must type"
+  if @notice_hook and signal_data.include? "Ident broken or disabled, to continue to connect you must type"
     server = signal.split(',')[0]
     passwd = signal_data.split(" ")[-1]
     Weechat.print('',"Sending UnderNet quote pass: #{passwd}")
